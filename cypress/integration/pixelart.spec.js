df56df520da0df942331038aa7a8f90da309f81b
Christian Glissmann <cgliss2000@gmail.com>
2021-08-11 06:01:42 +0200

    refactor: implemented e2e testing, eslint+prettier

---
+++ cypress/integration/pixelart.spec.js
@@ -0,0 +1,43 @@
+describe('PixelArt', () => {
+  beforeEach('visit', () => {
+    cy.visit('/');
+  });
+
+  it('init field with input', () => {
+    cy.get('[data-test=input-size]')
+      .should('exist')
+      .type('10')
+      .type(' {enter}');
+
+    cy.get('[data-test=palette]').should('exist');
+    cy.get('[data-test=palette-square]')
+      .should('exist')
+      .should('have.length', '10');
+
+    cy.get('[data-test=art-field]').should('exist');
+    cy.get('[data-test=field-square]')
+      .should('exist')
+      .should('have.length', '100');
+  });
+  it('init field with button', () => {
+    cy.get('[data-test=dim-btn1]').should('exist').click();
+    cy.get('[data-test=palette-square]')
+      .should('exist')
+      .should('have.length', '8');
+    cy.get('[data-test=field-square]')
+      .should('exist')
+      .should('have.length', '64');
+  });
+
+  it('change color', () => {
+    cy.get('[data-test=dim-btn1]').should('exist').click();
+    cy.get('[data-test=palette-square]').click({ multiple: true });
+    cy.get('[data-test=palette-square]').first().click();
+    cy.get('[data-bg-color]').then(($color) => {
+      cy.get('[data-test=field-square]')
+        .first()
+        .click()
+        .should('have.css', 'background-color');
+    });
+  });
+});
--- src/script.js
+++ src/script.js
@@ -1,27 +1,49 @@
-let globalColor = "";
+let globalColor = '';
 let globalSize = null;
+const dimensionBtn = document.querySelectorAll('.dimension-btn');
+const size = document.querySelector('#input-size');
+// initializing the dimension - remove Eventlistener after init
 
-const size = document.querySelector("#input-size");
-size.addEventListener("keydown", initSite);
+size.addEventListener('keydown', keyInit);
 
-function initSite(e) {
+function keyInit(e) {
   if (e.keyCode === 13) {
-    console.log(typeof event.target.value);
     globalSize = event.target.value;
     initPalette(event.target.value);
     initField(event.target.value);
-    event.target.removeEventListener("keydown", initSite);
-    event.target.value = "";
+    event.target.removeEventListener('keydown', initSite);
+    for (let i = 0; i < dimensionBtn.length; i++) {
+      dimensionBtn[i].removeEventListener('click', buttonInit);
+    }
+  }
+}
+for (let i = 0; i < dimensionBtn.length; i++) {
+  dimensionBtn[i].addEventListener('click', buttonInit);
+}
+
+function buttonInit() {
+  globalSize = event.target.value;
+  initPalette(event.target.value);
+  initField(event.target.value);
+  event.target.removeEventListener('keydown', initSite);
+  for (let i = 0; i < dimensionBtn.length; i++) {
+    dimensionBtn[i].removeEventListener('click', buttonInit);
   }
 }
 
+function initSite(value) {
+  globalSize = value;
+  initPalette(value);
+  initField(value);
+}
+
 function initPalette(size) {
-  const palette = document.querySelector("#palette");
-  let gridRows = "";
+  const palette = document.querySelector('#palette');
+  let gridRows = '';
   for (let i = 0; i < size - 1; i++) {
-    gridRows = gridRows + "1fr ";
+    gridRows = gridRows + '1fr ';
   }
-  gridRows = gridRows + "1fr";
+  gridRows = gridRows + '1fr';
   palette.style.gridTemplateColumns = gridRows;
 
   for (let j = 0; j < size; j++) {
@@ -30,31 +52,33 @@ function initPalette(size) {
 }
 
 function renderPalette(parent, fieldx) {
-  square = document.createElement("div");
-  square.classList = "palette-square";
-  height = 30 / globalSize + "rem";
-  square.style.height = height;
-  square.setAttribute("x-axis", fieldx);
-  square.setAttribute("y-axis", 0);
+  const square = document.createElement('div');
+  square.classList = 'palette-square';
+  square.setAttribute('data-test', 'palette-square');
+  const paletteHeight = 30 / globalSize + 'rem';
+  square.style.height = paletteHeight;
+  square.setAttribute('x-axis', fieldx);
+  square.setAttribute('y-axis', 0);
   const backgroundColor = randomColor();
   square.style.backgroundColor = backgroundColor;
-  square.addEventListener("click", function () {
+  square.setAttribute('data-bg-color', backgroundColor);
+  square.addEventListener('click', function () {
     globalColor = event.target.style.backgroundColor;
-    console.log(globalColor);
   });
   parent.appendChild(square);
 }
 
 function initField(size) {
-  const field = document.querySelector(".art-field");
-  let gridRows = "";
+  const field = document.querySelector('.art-field');
+  let gridRows = '';
   for (let i = 0; i < size - 1; i++) {
-    gridRows = gridRows + "1fr ";
+    gridRows = gridRows + '1fr ';
   }
-  gridRows = gridRows + "1fr";
-  console.log(gridRows);
+  gridRows = gridRows + '1fr';
+
   field.style.gridTemplateColumns = gridRows;
   field.style.gridTemplateRows = gridRows;
+
   for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
       renderSquare(field, i, j);
@@ -63,46 +87,42 @@ function initField(size) {
 }
 
 function renderSquare(parent, fieldx, fieldy) {
-  square = document.createElement("div");
-  square.classList = "square";
-  square.id = "square";
-  square.setAttribute("x-axis", fieldx);
-  square.setAttribute("y-axis", fieldy);
-  square.addEventListener("click", coloredSquare);
+  const square = document.createElement('div');
+  square.classList = 'square';
+  square.id = 'square';
+  square.setAttribute('x-axis', fieldx);
+  square.setAttribute('y-axis', fieldy);
+  square.setAttribute('data-test', 'field-square');
+  square.addEventListener('click', coloredSquare);
   parent.appendChild(square);
 }
 
-//generate new colors
-const newColorButton = document.querySelector("#new-color");
-newColorButton.addEventListener("click", generateNewColor);
+// generate new colors
+const newColorButton = document.querySelector('#new-color');
+newColorButton.addEventListener('click', generateNewColor);
 
 function generateNewColor() {
-  colors = document.querySelectorAll(".palette-square");
+  const colors = document.querySelectorAll('.palette-square');
   colors.forEach(function (element) {
     const randomBGColor = randomColor();
     element.style.backgroundColor = randomBGColor;
   });
 }
 
+// coloring the clicked square
 function coloredSquare() {
-  cb = document.querySelector("#checkbox");
-  console.log(cb.checked);
+  const cb = document.querySelector('#checkbox');
   if (cb.checked) {
-    console.log(event.target);
     fillTool(event.target);
   } else {
     event.target.style.backgroundColor = globalColor;
-    const x = event.target.getAttribute("x-axis");
-    const y = event.target.getAttribute("y-axis");
-    console.log(x + " " + y);
   }
 }
 
+// coliring via fill tool
 function fillTool(event) {
-  xAxis = event.getAttribute("x-axis");
-  yAxis = event.getAttribute("y-axis");
-  console.log(xAxis + " " + yAxis);
-  console.log(globalSize - 1);
+  const xAxis = event.getAttribute('x-axis');
+  const yAxis = event.getAttribute('y-axis');
 
   if (xAxis == 0) {
     if (yAxis == 0) {
@@ -116,7 +136,6 @@ function fillTool(event) {
       getField(1, globalSize - 2);
       getField(1, globalSize - 1);
     } else {
-      console.log(xAxis + " " + yAxis);
       for (let i = -1; i < 2; i++) {
         getField(0, yAxis - i);
         getField(1, yAxis - i);
@@ -134,7 +153,7 @@ function fillTool(event) {
       getField(globalSize - 2, globalSize - 2);
       getField(globalSize - 2, globalSize - 1);
     } else {
-      console.log(xAxis + " " + yAxis);
+      console.log(xAxis + ' ' + yAxis);
       for (let i = -1; i < 2; i++) {
         getField(globalSize - 1, yAxis - i);
         getField(globalSize - 2, yAxis - i);
@@ -159,14 +178,11 @@ function fillTool(event) {
   }
 }
 function getField(x, y) {
-  console.log(x);
-  allSquare = document.querySelectorAll("#square");
-  for (i = 0; i < allSquare.length; i++) {
-    const xAxis = allSquare[i].getAttribute("x-axis");
-    const yAxis = allSquare[i].getAttribute("y-axis");
-    //console.log(xAxis + " " + x + "y" + y + yAxis);
+  const allSquare = document.querySelectorAll('#square');
+  for (let i = 0; i < allSquare.length; i++) {
+    const xAxis = allSquare[i].getAttribute('x-axis');
+    const yAxis = allSquare[i].getAttribute('y-axis');
     if (xAxis == x && yAxis == y) {
-      console.log("test");
       allSquare[i].style.backgroundColor = globalColor;
       return;
     }
@@ -178,7 +194,7 @@ function randomColor() {
   const red = randomHexNumber();
   const green = randomHexNumber();
   const blue = randomHexNumber();
-  return ("#" + red + green + blue).toUpperCase();
+  return ('#' + red + green + blue).toUpperCase();
 }
 
 function randomNumber(min, max) {
@@ -189,7 +205,25 @@ function randomNumber(min, max) {
 function randomHexNumber() {
   let hex = randomNumber(0, 255).toString(16);
   if (hex.length === 1) {
-    hex = "0" + hex;
+    hex = '0' + hex;
   }
   return hex;
+}
+
+const refreshBtn = document.querySelector('.refresh-btn');
+refreshBtn.addEventListener('click', refreshSite);
+
+function refreshSite() {
+  for (let i = 0; i < dimensionBtn.length; i++) {
+    dimensionBtn[i].addEventListener('click', buttonInit);
+  }
+  size.addEventListener('keydown', keyInit);
+  const delPalette = document.querySelectorAll('.palette-square');
+  for (let i = 0; i < delPalette.length; i++) {
+    delPalette[i].remove();
+  }
+  const delSquare = document.querySelectorAll('.square');
+  for (let i = 0; i < delSquare.length; i++) {
+    delSquare[i].remove();
+  }
 }
